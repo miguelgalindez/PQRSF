649dee7be98d90eb6b58cf98b4d123534fe611aa
Miguel Angel <miguel@jessie>
2016-11-01 18:11:15 -0500

    Se agrego Seed para la tabla Users

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'id' => 'miguelgalindez@unicauca.edu.co',
+            'name' => 'Miguel Angel Galíndez Muñoz',
+            'rol' => 'Administrador',
+            'fechaInicio' => date('Y-m-d H:i:s'),
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+    }
+}
