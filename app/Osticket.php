5c91e14956cc3defb312cbeccf6135de11cb8f4f
Miguel Angel <miguel@jessie>
2016-11-06 12:01:17 -0500

    Desarrollada la funcion crearUsuario en osticketdb -modelo-

--- app/Osticket.php
+++ app/Osticket.php
@@ -91,7 +91,86 @@ class Osticket extends Model
         }
     }
 
-    public static function crearTicket($idDependencia, $idFuncionario, $fechaVencimiento, $idPrioridad, $subject){
+    public function usuarioExiste($email){
+        $db=DB::connection('osticketdb');
+        
+        $cont=$db->table('ost_user_email')
+                    ->where('address', $email)
+                    ->select('address')
+                    ->count();
+
+        return $cont>0;
+    }
+
+    public function crearUsuario($nombres, $email, $telefono){
+
+        $now=date('Y-m-d H:i:s');
+        $db=DB::connection('osticketdb');
+
+        $idUsuario=$db->table('ost_user')
+                            ->insertGetId([
+                                'org_id' => 0,
+                                'default_email_id' => 0,
+                                'status' => 0,
+                                'name' => $nombres,
+                                'created' => $now,
+                                'updated' => $now
+        ]);
+
+        $idEmail=$db->table('ost_user_email')
+                            ->insertGetId([
+                                'user_id' => $idUsuario,
+                                'address' => $email
+        ]);
+
+        $db->table('ost_user')
+                ->where('id', $idUsuario)
+                ->update(['default_email_id' => $idEmail]);
+
+            // define los datos del usuario (form_id=1) 
+            //    objectId hace referencia al id del usuario
+       $idEntrada=$db->table('ost_form_entry')
+                        ->insertGetId([
+                            'form_id' => 1,
+                            'object_id' => $idUsuario,
+                            'object_type' => 'U',
+                            'sort' => 1,
+                            'created' => $now,
+                            'updated' => $now
+        ]);
+
+        // se le asigna el numero de TELEFONO a la entrada recien creada
+
+        $db->table('ost_form_entry_values')
+                        ->insert([
+                            'entry_id' => $idEntrada,
+                            'field_id' => 3,
+                            'value' => $telefono,
+        ]);
+
+        // se le asigna las NOTAS INTERNAS a la entrada recien creada. OJO por ahora es NULL
+        $db->table('ost_form_entry_values')
+                        ->insert([
+                            'entry_id' => $idEntrada,
+                            'field_id' => 4,
+                            // 'value' => $notasInternas, // Por ahora sera NULO 
+                                                        
+        ]);
+
+        // Osticket por defecto tambien hace la siguiente inserción
+        $db->table('ost__search')
+                        ->insert([
+                            'object_type' => 'U',
+                            'object_id' => $idUsuario,
+                            'title' => $nombres,
+                            'content' => ''
+        ]);
+
+        return $idUsuario;
+    }
+
+
+    public static function crearTicket($nombrePersona,$emailPersona, $telefonoPersona, $idDependencia, $idFuncionario, $fechaVencimiento, $idPrioridad, $subject){
         $db=DB::connection('osticketdb');
 
         $sigNumeroTicket=Self::obtnSigNumeroTicket();
@@ -100,6 +179,16 @@ class Osticket extends Model
         
         try{
             $db->beginTransaction();
+
+            if($this->usuarioExiste()){
+                //  TODO
+                //  $idUsuario= xxxxx   obtener el id del usuario
+            }
+            else{
+                $idUsuario=$this->crearUsuario($nombrePersona, $emailPersona, $telefonoPersona);
+            }
+
+
             $idTicket=$db->table('ost_ticket')
                             ->insertGetId([
                                 'number' => $sigNumeroTicket,
