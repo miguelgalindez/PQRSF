1481c5237d0b1e818258d983c74edcd58b506428
Miguel Angel <miguel@jessie>
2016-11-07 17:07:31 -0500

    Admin Dashboard integrada

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,6 +1,6 @@
 @extends('admin.master')
-@section('title', 'Index')
+@section('title', 'Exito')
 
 @section('content')
-
+	<p>Exito mundo</p>
 @endsection
---
+++ resources/views/admin/partial/footer.blade.php
@@ -0,0 +1,42 @@
+	
+
+	</section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>Version</b> 2.3.7
+    </div>
+    <strong>Copyright &copy; 2014-2016 <a href="http://almsaeedstudio.com">Almsaeed Studio</a>.</strong> All rights
+    reserved.
+</footer>
+
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 2.2.3 -->
+<script src="{{ asset('js/jquery-3.1.1.min.js') }}"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src=" {{ asset('js/bootstrap.min.js') }}"></script>
+<!-- FastClick -->
+<script src="{{ asset('plugins/fastclick/fastclick.js') }}"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('js/AdminLTE/app.min.js') }}"></script>
+<!-- Sparkline -->
+<script src="{{ asset('plugins/sparkline/jquery.sparkline.min.js') }}"></script>
+<!-- jvectormap -->
+<script src="{{ asset('plugins/jvectormap/jquery-jvectormap-1.2.2.min.js') }}"></script>
+<script src="{{ asset('plugins/jvectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
+<!-- SlimScroll 1.3.0 -->
+<script src="{{ asset('plugins/slimScroll/jquery.slimscroll.min.js') }}"></script>
+<!-- ChartJS 1.0.1 -->
+<script src="{{ asset('plugins/chartjs/Chart.min.js') }}"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) 
+<script src="dist/js/pages/dashboard2.js"></script>
+<!-- AdminLTE for demo purposes 
+<script src="dist/js/demo.js"></script>
+-->
+</body>
+</html>
