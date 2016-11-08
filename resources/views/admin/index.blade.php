5662a3d736845cfe6965e995a709143691da0573
root <root@jessie>
2016-11-08 12:04:59 -0500

    AdminLTE intgrada

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,5 +2,72 @@
 @section('title', 'Exito')
 
 @section('content')
-	<p>Exito mundo</p>
+	<section class="content">
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+              <h3>15</h3>
+
+              <p>PQRSFs vencidas</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-nuclear"></i>
+            </div>
+            <a href="#" class="small-box-footer">Más información <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-orange">
+            <div class="inner">
+              <h3>20</h3>
+
+              <p>PQRSFs pŕoximas a vencerse</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-alert-circled"></i>
+            </div>
+            <a href="#" class="small-box-footer">Más información <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-blue">
+            <div class="inner">
+              <h3>10</h3>
+
+              <p>PQRSFs que se estan atendiendo</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-android-people"></i>
+            </div>
+            <a href="#" class="small-box-footer">Mas información <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3>65</h3>
+
+              <p>PQRSFs atendidas</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-android-checkmark-circle"></i>
+            </div>
+            <a href="#" class="small-box-footer">Más información <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <!-- ./col -->
+      </div>
+      <!-- /.row -->
+    </section>
 @endsection
--- resources/views/admin/partial/leftSidebar.blade.php
+++ resources/views/admin/partial/leftSidebar.blade.php
@@ -30,34 +30,47 @@
       <!-- /.search form -->
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu">
-        <li class="header">MAIN NAVIGATION</li>
-        <li class="active treeview">
+
+        <li class="header">Panel de control</li>
+        
+        <li>
+          <a href="/admin">
+            <i class="fa fa-dashboard"></i> 
+            <span>Inicio</span>
+          </a>
+        </li>  
+
+        <li class="treeview">
           <a href="#">
-            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+            <i class="fa fa-files-o"></i>
+            <span>Consultas</span>
             <span class="pull-right-container">
               <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-            <li class="active"><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+            <li><a href=""><i class="fa fa-circle-o"></i>PQRSFs vencidas</a></li>
+            <li><a href=""><i class="fa fa-circle-o"></i>PQRSFs proximas a vencerse</a></li>
+            <li><a href=""><i class="fa fa-circle-o"></i>PQRSFs por estado</a></li>        
+            <li><a href=""><i class="fa fa-circle-o"></i>Todas las PQRSFs</a></li>
           </ul>
         </li>
+
         <li class="treeview">
           <a href="#">
             <i class="fa fa-files-o"></i>
-            <span>Layout Options</span>
+            <span>Acciones</span>
             <span class="pull-right-container">
-              <span class="label label-primary pull-right">4</span>
+              <i class="fa fa-angle-left pull-right"></i>
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
-            <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i> Boxed</a></li>
-            <li><a href="pages/layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
-            <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
+            <li><a href=""><i class="fa fa-circle-o"></i>Registrar PQRSFs</a></li>
+            <li><a href=""><i class="fa fa-circle-o"></i>Radicar PQRSFs</a></li>
+            <li><a href=""><i class="fa fa-circle-o"></i>Direccionar PQRSFs</a></li>                  
           </ul>
         </li>
+
         <li>
           <a href="pages/widgets.html">
             <i class="fa fa-th"></i> <span>Widgets</span>
